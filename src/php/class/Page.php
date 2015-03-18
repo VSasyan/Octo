0379cb7edbc1bf1e26cefe307f6a693d6c3afad0
Nerisson <jean-paul.sky@laposte.net>
2015-03-18 16:25:12 +0100

    MAJ API PHP
    export des articles d'un portail

--- src/php/class/Page.php
+++ src/php/class/Page.php
@@ -35,17 +35,16 @@ class Page extends Lien{
     
     public function getFullQuery(){
         $sql = "INSERT INTO page(id, lon, lat, nb_langue, nb_visite, longueur, debut_annee, debut_mois, "
-                . "debut_jour, fin_annee, fin_mois, fin_jour, date_MAJ, importance, distance_portail, id_portail )"
+                . "debut_jour, fin_annee, fin_mois, fin_jour, date_MAJ, type_infobox, distance_portail, id_portail )"
                 . "VALUES (".$this->json->id.", ".$this->json->lon.", ".$this->json->lat.", "
                 . $this->json->nb_langue.", ".$this->json->nb_visite.", ".$this->json->longueur.", "
                 . $this->json->debut_annee.", ".$this->json->debut_mois.", ".$this->json->debut_jour.", "
                 . $this->json->fin_annee.", ".$this->json->fin_mois.", ".$this->json->fin_jour.", "
-                . "NOW(), ".$this->json->importance.", ".$this->json->distance_Portail.", "
+                . "NOW(), ".$this->json->type_infobox.", ".$this->json->distance_Portail.", "
                 . $this->json->portail_id.");\n";
         
         $sql = $sql.parent::getQueryAdd();
         return $sql;
     }
-    
     
 }
