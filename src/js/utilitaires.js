bd143a421689815304ffc0c6d6aeaba64a20359d
VSasyan <valentin.sasyan@gmail.com>
2015-03-21 18:05:29 +0100

    Organisation des répertoires

--- src/js/utilitaires.js
+++ src/js/utilitaires.js
@@ -1,1 +1,20 @@
 var html_chargement = '<div class="center"><img src="../../image/load.gif" alt="Chargement en cours..." title="Chargement en cours..."/></div>';
+
+function getMediane(values) {
+    values.sort( function(a,b) {return a - b;} );
+    var half = Math.floor(values.length/2);
+    if(values.length % 2) {return values[half];}
+    else {return (values[half-1] + values[half]) / 2.0;}
+}
+
+Array.prototype.shuffle = function () {
+	return this[Math.floor(Math.random() * this.length)]
+}
+
+function rand(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function RandR(min, max) {
+	return Math.random() * (max - min) + min;
+}
