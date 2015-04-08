f18d7b9002650e48a5846b4b1c4be2dcf0f494ba
VSasyan <valentin.sasyan@gmail.com>
2015-04-08 16:27:06 +0200

    MAJ Viewer v0.5

---
+++ src/php/viewer/viewer.php
@@ -0,0 +1,27 @@
+<article id="action" class="loading">
+	<div id="liste"></div>
+	<div id="timemap">
+		<div id="top">
+			<div id="licence"><a href="#viewSource">Source</a></div>
+			<div id="anim" class="center">
+				<input type="number" id="duration" value="30000"/>
+				<button id="animate">Animation !!!</button>
+			</div>
+		</div>
+		<div id="timelinecontainer">
+			<div id="timeline"></div>
+		</div>
+		<div id="mapcontainer">
+			<div id="map"></div>
+		</div>
+	</div>
+	<div id="loading" class="center">
+		<img src="../../image/load.gif" alt="Chargement en cours..." title="Chargement en cours...">
+	</div>
+</article>
+
+<div id="viewSource">
+	<a href="#" id="fermer">&times;</a>
+	<div id="htmlSource"></div>
+</div>
+<div id="cover"></div>
