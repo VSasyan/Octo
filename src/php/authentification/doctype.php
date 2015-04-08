5f5015d4d7cc627c9b7fb03e0d7baedc4cb1e334
Adélaïde <adela_l@hotmail.fr>
2015-04-08 16:25:45 +0200

    Authentification

---
+++ src/php/authentification/doctype.php
@@ -0,0 +1,12 @@
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8" />
+    <meta name="author" content="Adélaïde Lorieux" />
+    <meta name="description" content="Cicérow" />
+     <link rel="stylesheet" type="text/css" href="normalize.css" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
+		<title>Projet Cicérow</title>
---
+++ src/php/authentification/setSession.php
@@ -0,0 +1,14 @@
+<?php
+	session_start();
+	if (isset($_GET['json'])) {
+		$data = json_decode($_GET['json']);
+		$_SESSION['user']["login"]) = $data['login'];
+		$_SESSION['user']["idU"]) = $data['idU'];
+		$_SESSION['user']["role"]) = $data['role'];
+		$_SESSION['user']["roleType"]) = $data['roleType'];
+		echo '{"valide":true}';
+	} else {
+		echo '{"valide":false}';
+	}
+
+?>
