3b124d1d7a3dce736cfaa01d108fa35ff0840a55
Adélaïde <adela_l@hotmail.fr>
2015-04-13 17:29:16 +0200

    MAJ menu.php

--- src/php/authentification/menu.php
+++ src/php/authentification/menu.php
@@ -1,15 +1,15 @@
 
 <div id="menu">                      
-<ul>
-  <li><a href=a.php>Mes cartes</a></li>
-  <li><a href=b.php>Créer une carte</a></li>
-  <li><a href=b.php>Editer une carte</a></li>
-  <li><a href=b.php>Personnaliser une carte</a></li>
-  <?php
-  if ($_SESSION['user']["role"]>1){
-	echo '<li><a href=b.php>Ajouter un portail</a></li>';
-   }
-   ?>
-  <li><a href=auth.php?deconnect>Déconnexion</a></li>
-</ul>        
+	<ul>
+	  <li><a href='index.php?page=viewer&type=mescartes'>Mes cartes</a></li>
+	  <li><a href='index.php?page=personnaliser&type=creer'>Créer une carte</a></li>
+	  <li><a href='index.php?page=personnaliser&type=editer'>Editer une carte</a></li>
+	  <li><a href='index.php?page=personnaliser&type=perso'>Personnaliser une carte</a></li>
+	  <?php
+	  if ($_SESSION['user']["role"]>1){
+		echo '<li><a href="index.php?page=edit">Gérer les portails</a></li>';
+	   }
+	   ?>
+	  <li><a href=index.php?deconnect>Déconnexion</a></li>
+	</ul>        
 </div>
