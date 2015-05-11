de8942822d11e24a3d0eede53b784e8a473a3d78
VSasyan <valentin.sasyan@gmail.com>
2015-05-11 23:57:18 +0200

    MAJ thèmes

--- src/php/viewer/themes.php
+++ src/php/viewer/themes.php
@@ -4,6 +4,7 @@
 		'themes' => array(
 			array(
 				'nom' => 'defaut',
+				'titre' => 'Rouge',
 				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/red-dot.png",
 				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/red-circle.png",
 				'iconSize' => [32,32],
@@ -11,20 +12,7 @@
 			),
 			array(
 				'nom' => 'defautGrand',
-				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/red-dot.png",
-				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/red-circle.png",
-				'iconSize' => [64,64],
-				'color' => '#FE766A'
-			),
-			array(
-				'nom' => 'red',
-				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/red-dot.png",
-				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/red-circle.png",
-				'iconSize' => [32,32],
-				'color' => '#FE766A'
-			),
-			array(
-				'nom' => 'redGrand',
+				'titre' => 'Rouge (Important)',
 				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/red-dot.png",
 				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/red-circle.png",
 				'iconSize' => [64,64],
@@ -32,6 +20,7 @@
 			),
 			array(
 				'nom' => 'blue',
+				'titre' => 'Blue',
 				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/blue-dot.png",
 				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/blue-circle.png",
 				'iconSize' => [32,32],
@@ -39,6 +28,7 @@
 			),
 			array(
 				'nom' => 'blueGrand',
+				'titre' => 'Blue (Important)',
 				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/blue-dot.png",
 				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/blue-circle.png",
 				'iconSize' => [64,64],
@@ -46,6 +36,7 @@
 			),
 			array(
 				'nom' => 'purple',
+				'titre' => 'Violet',
 				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/purple-dot.png",
 				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/purple-circle.png",
 				'iconSize' => [32,32],
@@ -53,6 +44,7 @@
 			),
 			array(
 				'nom' => 'purpleGrand',
+				'titre' => 'Violet (Important)',
 				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/purple-dot.png",
 				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/purple-circle.png",
 				'iconSize' => [64,64],
@@ -60,6 +52,7 @@
 			),
 			array(
 				'nom' => 'ltblue',
+				'titre' => 'Bleu léger',
 				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/ltblue-dot.png",
 				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/ltblue-circle.png",
 				'iconSize' => [32,32],
@@ -67,6 +60,7 @@
 			),
 			array(
 				'nom' => 'ltblueGrand',
+				'titre' => 'Bleu léger (Important)',
 				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/ltblue-dot.png",
 				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/ltblue-circle.png",
 				'iconSize' => [64,64],
@@ -74,6 +68,7 @@
 			),
 			array(
 				'nom' => 'green',
+				'titre' => 'Vert',
 				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/green-dot.png",
 				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/green-circle.png",
 				'iconSize' => [32,32],
@@ -81,6 +76,7 @@
 			),
 			array(
 				'nom' => 'greenGrand',
+				'titre' => 'Vert (Important)',
 				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/green-dot.png",
 				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/green-circle.png",
 				'iconSize' => [64,64],
@@ -88,6 +84,7 @@
 			),
 			array(
 				'nom' => 'orange',
+				'titre' => 'Orange',
 				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/orange-dot.png",
 				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/orange-circle.png",
 				'iconSize' => [32,32],
@@ -95,6 +92,7 @@
 			),
 			array(
 				'nom' => 'orangeGrand',
+				'titre' => 'Orange (Important)',
 				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/orange-dot.png",
 				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/orange-circle.png",
 				'iconSize' => [64,64],
@@ -102,6 +100,7 @@
 			),
 			array(
 				'nom' => 'yellow',
+				'titre' => 'Jaune',
 				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/yellow-dot.png",
 				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/yellow-circle.png",
 				'iconSize' => [32,32],
@@ -109,6 +108,7 @@
 			),
 			array(
 				'nom' => 'yellowGrand',
+				'titre' => 'Jaune (Important)',
 				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/yellown-dot.png",
 				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/yellow-circle.png",
 				'iconSize' => [64,64],
@@ -116,6 +116,7 @@
 			),
 			array(
 				'nom' => 'pink',
+				'titre' => 'Rose',
 				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/pink-dot.png",
 				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/pink-circle.png",
 				'iconSize' => [32,32],
@@ -123,6 +124,7 @@
 			),
 			array(
 				'nom' => 'pinkGrand',
+				'titre' => 'Rose (Important)',
 				'icon' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/pinkn-dot.png",
 				'eventIconImage' => "http://www.google.com/intl/en_us/mapfiles/ms/icons/pink-circle.png",
 				'iconSize' => [64,64],
@@ -130,6 +132,7 @@
 			),
 			array(
 				'nom' => 'bataille',
+				'titre' => 'Bataille',
 				'icon' => "../image/theme/bataille_grand.png",
 				'eventIconImage' => "../image/theme/bataille_grand.png",
 				'iconSize' => [32,32],
@@ -137,14 +140,68 @@
 			),
 			array(
 				'nom' => 'batailleGrand',
+				'titre' => 'Bataille (Important)',
 				'icon' => "../image/theme/bataille_grand.png",
 				'eventIconImage' => "../image/theme/bataille_grand.png",
 				'iconSize' => [64,64],
 				'color' => '#000000'
+			),
+			array(
+				'nom' => 'navirephare',
+				'titre' => 'Navir ou Phare',
+				'icon' => "../image/theme/navire_phare.png",
+				'eventIconImage' => "../image/theme/navire_phare.png",
+				'iconSize' => [32,32],
+				'color' => '#000000'
+			),
+			array(
+				'nom' => 'navirephareGrand',
+				'titre' => 'Navir ou Phare (Important)',
+				'icon' => "../image/theme/navire_phare.png",
+				'eventIconImage' => "../image/theme/navire_phare.png",
+				'iconSize' => [64,64],
+				'color' => '#000000'
+			),
+			array(
+				'nom' => 'sitearcheologique',
+				'titre' => 'Site archéologique',
+				'icon' => "../image/theme/site_archeologique.png",
+				'eventIconImage' => "../image/theme/site_archeologique.png",
+				'iconSize' => [32,32],
+				'color' => '#000000'
+			),
+			array(
+				'nom' => 'sitearcheologiqueGrand',
+				'titre' => 'Site archéologique (Important)',
+				'icon' => "../image/theme/site_archeologique.png",
+				'eventIconImage' => "../image/theme/site_archeologique.png",
+				'iconSize' => [64,60],
+				'color' => '#000000'
+			),
+			array(
+				'nom' => 'gratteciel',
+				'titre' => 'Gratte-ciel',
+				'icon' => "../image/theme/gratte_ciel.png",
+				'eventIconImage' => "../image/theme/gratte_ciel.png",
+				'iconSize' => [32,40],
+				'color' => '#000000'
+			),
+			array(
+				'nom' => 'grattecielGrand',
+				'titre' => 'Gratte-ciel (Important)',
+				'icon' => "../image/theme/gratte_ciel.png",
+				'eventIconImage' => "../image/theme/gratte_ciel.png",
+				'iconSize' => [64,81],
+				'color' => '#000000'
 			)
 		),
 		'infobox' => array(
-			'conflit militaire' => 'bataille'
+			'conflitmilitaire' => 'bataille',
+			'navirephare' => 'navirephare',
+			'navire' => 'navirephare',
+			'phare' => 'navirephare',
+			'sitearcheologique' => 'sitearcheologique',
+			'gratteciel' => 'gratteciel'
 		)
 	);
 
